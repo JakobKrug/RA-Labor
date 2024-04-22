1b6129cf581481e51a5bc0ae116d08ee125b5396
JakobKrug <jakobkrug04@gmail.com>
2024-04-22 12:43:42 +0200

    finali folder structure

--- Versuch1/Testbenches/ALU/alu_linux.sh
+++ Versuch1/Testbenches/ALU/alu_linux.sh
@@ -11,8 +11,8 @@ ghdl -a ../../Komponenten/ALU/my_gen_xor.vhdl
 ghdl -a ../../Komponenten/ALU/my_gen_or.vhdl
 ghdl -a ../../Komponenten/ALU/my_gen_and.vhdl
 
-ghdl -a ../../my_alu.vhdl
+ghdl -a ../../Komponenten/ALU/my_alu.vhdl
 
 ghdl -a ../../Testbenches/ALU/my_alu_tb.vhdl
 ghdl -e my_alu_tb
-ghdl -r my_alu_tb --vcd=my_alu_tb.vcd --stop-time=100ns
+ghdl -r my_alu_tb --vcd=my_alu_tb.vcd --stop-time=1ms
