d7d89c5134d021f8dc9f6f659244814a3e1f04bd
JakobKrug <jakobkrug04@gmail.com>
2024-04-22 13:00:18 +0200

    edited folder structure

---
+++ gen_mux.vhdl
@@ -0,0 +1,21 @@
+-- Laboratory RA solutions/versuch1
+-- Sommersemester 24
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use work.constant_package.all;
+
+entity gen_mux is
+  -- begin solution:
+  -- end solution!!
+end entity gen_mux;
+
+architecture dataflow of gen_mux is
+-- begin solution:
+-- end solution!!
+end architecture dataflow;
