3d0b556c2dab8f4ff0ab78003e09da5fbe80e84d
JakobKrug <jakobkrug04@gmail.com>
2024-04-23 10:32:26 +0200

    finished versuch1.sh

--- Versuch1/versuch1.sh
+++ Versuch1/versuch1.sh
@@ -1,3 +1,3 @@
 (cd ../Testbenches/ALU && bash alu_linux.sh)
-#(cd ::/Testbenches/Register && bash register_linux.sh)
+(cd ::/Testbenches/Register && bash register_linux.sh)
 (cd ../Testbenches/MUX && bash mux_linux.sh)
