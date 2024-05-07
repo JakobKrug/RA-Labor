bbc4f591ac51fd02f93a141ed8337cea82b4f311
JakobKrug <jakobkrug04@gmail.com>
2024-05-07 11:16:01 +0200

    "added Nicos Register File"

--- Komponenten/Registerfile/register_file.vhdl
+++ Komponenten/Registerfile/register_file.vhdl
@@ -1,59 +1,64 @@
 -- Laboratory RA solutions/versuch2
 -- Sommersemester 24
 -- Group Details
--- Lab Date: 30.04.2024
--- 1. Participant First and Last Name: Jakob Benedikt Krug
--- 2. Participant First and Last Name: Nicolas Schmidt
+-- Lab Date:
+-- 1. Participant First and Last Name: Nicolas Schmidt 
+-- 2. Participant First and Last Name: Jakob Krug
 
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
   use ieee.math_real.all;
   use work.constant_package.all;
+  use work.types_package.all;
 
 entity register_file is
--- begin solution:
-generic (
+    generic(
     word_width : integer := WORD_WIDTH;
     adr_width : integer := REG_ADR_WIDTH;
     reg_amount : integer := 2**REG_ADR_WIDTH
-);
-port (
+    );
+    port(
     pi_clk : in std_logic := '0';
     pi_rst : in std_logic := '0';
-    pi_readRegAddr1 : in std_logic_vector(adr_width - 1 downto 0) := (others => '0');
-    pi_readRegAddr2 : in std_logic_vector(adr_width - 1 downto 0) := (others => '0');
-    pi_writeRegAddr : in std_logic_vector(adr_width - 1 downto 0) := (others => '0');
-    pi_writeRegData : in std_logic_vector(word_width - 1 downto 0) := (others => '0');
+    pi_readRegAddr1 : in std_logic_vector(REG_ADR_WIDTH - 1 downto 0) := (others => '0');
+    pi_readRegAddr2 : in std_logic_vector(REG_ADR_WIDTH - 1 downto 0) := (others => '0');
+    pi_writeRegAddr : in std_logic_vector(REG_ADR_WIDTH - 1 downto 0) := (others => '0');
+    pi_writeRegData : in std_logic_vector(WORD_WIDTH - 1 downto 0) := (others => '0');
     pi_writeEnable : in std_logic := '0';
-    po_readRegData1 : out std_logic_vector(word_width - 1 downto 0) := (others => '0');
-    po_readRegData2 : out std_logic_vector(word_width - 1 downto 0) := (others => '0')
-);
-   -- end solution!!
+    po_readRegData1 : out std_logic_vector(WORD_WIDTH - 1 downto 0) := (others => '0'); 
+    po_readRegData2 : out std_logic_vector(WORD_WIDTH - 1 downto 0) := (others => '0');
+    po_registerOut : out registermemory := (others => (others => '0'))
+    );
 end entity register_file;
 
 architecture arc of register_file is
--- begin solution:
-type reg_array is array (0 to reg_amount - 1) of std_logic_vector(word_width - 1 downto 0);
-signal regs : reg_array := (others => (others => '0'));
-constant REG_0 : std_logic_vector(word_width - 1 downto 0) := (others => '0');
+    signal s_regs : registermemory := (others => (others => '0'));
+    signal s_init : boolean := false;
+    constant REG_0 : std_logic_vector(word_width - 1 downto 0) := (others => '0');
+    
 
 begin
-    process(pi_clk, pi_rst) is
+    process(pi_clk, pi_rst, pi_writeEnable)
         begin
-        if rising_edge(pi_clk) then
-            po_readRegData1 <= regs(to_integer(unsigned(pi_readRegAddr1)));
-            po_readRegData2 <= regs(to_integer(unsigned(pi_readRegAddr2)));
-            if (pi_writeEnable = '1') then
-                regs(to_integer(unsigned(pi_writeRegAddr))) <= pi_writeRegData;
+            if not s_init then 
+            s_regs(1) <= std_logic_vector(to_unsigned(9, WORD_WIDTH));
+            s_regs(2) <= std_logic_vector(to_unsigned(8, WORD_WIDTH));
+            s_init <= true;
             end if;
-            regs(0) <= REG_0;
-        end if;
-        if (pi_rst = '1') then
-            for i in 1 to reg_amount - 1 loop
-                regs(i) <= regs(0);
-            end loop;
-        end if;  
-    end process;
-   -- end solution!!
+            if rising_edge(pi_clk) then
+                    po_readRegData1 <= s_regs(to_integer(unsigned(pi_readRegAddr1))); 
+                    po_readRegData2 <= s_regs(to_integer(unsigned(pi_readRegAddr2)));
+                if pi_writeEnable = '1' then 
+                    s_regs(to_integer(unsigned(pi_writeRegAddr))) <= pi_writeRegData;
+                end if;
+              s_regs(0) <= REG_0;
+            end if;
+                if (pi_rst = '1') then
+                  for i in 1 to reg_amount - 1 loop
+                    s_regs(i) <= s_regs(0);
+                end loop; 
+            end if;
+            po_registerOut <= s_regs;
+        end process;
 end architecture arc;
