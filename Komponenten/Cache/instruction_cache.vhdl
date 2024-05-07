03addfd9240af1719fb25b7c968c07077d87f470
j k <jk108@tiprak03.informatik.uni-ulm.de>
2024-05-07 11:06:38 +0200

    Final Final Versuch3

---
+++ Komponenten/Cache/instruction_cache.vhdl
@@ -0,0 +1,53 @@
+-- Laboratory RA solutions/versuch3
+-- Sommersemester 24
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+
+
+-- ========================================================================
+-- Author:       Marcel Riess
+-- Last updated: 30.04.2024
+-- Description:  Generic instruction cache (read only) with debug port,
+--               to allow writing data in testbenches
+-- ========================================================================
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.math_real.all;
+  use work.constant_package.all;
+  use work.util_functions_package.all;
+  use work.types_package.all;
+
+entity instruction_cache is
+  generic (
+    adr_width : integer := ADR_WIDTH; -- Address Bus width of instruction memory (in RISCVI: 32)
+    mem_size  : integer := 2 ** 10      -- Size of instruction cache
+  );
+  port (
+    pi_adr       : in    std_logic_vector(adr_width - 1 downto 0)  := (others => '0');             -- Adress of the instruction to select
+    pi_clk      : in    std_logic;
+    pi_instructionCache :in memory :=(others => (others => '0'));
+    po_instruction       : out   std_logic_vector(WORD_WIDTH - 1 downto 0) := (others => '0')              -- Selected instruction
+  );
+end entity instruction_cache;
+
+architecture behavior of instruction_cache is
+
+  signal s_input      : std_logic_vector(ADR_WIDTH - 1 downto 0) := (others => '0');
+  signal instructions : memory := (others => (others => '0'));
+
+begin
+  instructions<=pi_instructionCache;
+  process (pi_clk) is
+  begin
+
+    if rising_edge(pi_clk) then
+      po_instruction <= instructions(to_integer(unsigned(pi_adr)));
+    end if;
+
+  end process;
+
+end architecture behavior;
---
+++ Komponenten/Register/ControlWordRegister.vhdl
@@ -0,0 +1,56 @@
+-- Laboratory RA solutions/versuch3
+-- Sommersemester 24
+-- Group Details
+-- Lab Date:
+-- 1. Participant First and Last Name: 
+-- 2. Participant First and Last Name:
+ 
+ 
+-- coding conventions
+-- g_<name> Generics
+-- p_<name> Ports
+-- c_<name> Constants
+-- s_<name> Signals
+-- v_<name> Variables
+
+-- ========================================================================
+-- Author:       Niklas Gutsmiedl
+-- Last updated: 02.2024
+-- Description:  Register to hold signals of type controlWord,
+--               as defined in Types_Package.vhdl. Used as phase registers  
+--               for the control path in the RV pipeline
+-- ========================================================================
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.MATH_REAL.all;
+use work.Constant_Package.all;
+use work.Types_Package.all;
+
+entity ControlWordRegister is
+
+  port (
+    pi_rst : in std_logic;
+    pi_clk : in std_logic;
+    pi_controlWord : in controlWord := CONTROL_WORD_INIT; -- incoming control word
+    po_controlWord : out controlWord := CONTROL_WORD_INIT -- outgoing control word
+  );
+end ControlWordRegister;
+
+architecture behavior of ControlWordRegister is
+  signal s_controlWord : controlWord := CONTROL_WORD_INIT;
+begin
+
+  process (pi_clk,pi_rst)
+    
+  begin
+
+    if (pi_rst) then
+      s_controlWord <= CONTROL_WORD_INIT;
+    elsif falling_edge (pi_clk) then
+      s_controlWord <= pi_controlWord; -- update register contents on falling clock edge
+    end if;
+  end process;
+
+  po_controlWord <= s_controlWord;
+end behavior;
