9701249f7f5e3ce1b20b13a65d1008f6a6637058
JakobKrug <jakobkrug04@gmail.com>
2024-05-09 23:57:33 +0200

    added versuch2 files

--- Testbenches/ALU/alu_linux.sh
+++ Testbenches/ALU/alu_linux.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 rm work-*.cf
 
-ghdl -a ../../Packages/Constant_Package.vhdl
+ghdl -a --std=08 ../../Packages/Constant_Package.vhdl
+ 
+ghdl -a --std=08 ../../Komponenten/ALU/my_shifter.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_half_adder.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_full_adder.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_gen_n_bit_full_adder.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_gen_xor.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_gen_or.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_gen_and.vhdl
 
-ghdl -a ../../Komponenten/ALU/my_shifter.vhdl
-ghdl -a ../../Komponenten/ALU/my_half_adder.vhdl
-ghdl -a ../../Komponenten/ALU/my_full_adder.vhdl
-ghdl -a ../../Komponenten/ALU/my_gen_n_bit_full_adder.vhdl
-ghdl -a ../../Komponenten/ALU/my_gen_xor.vhdl
-ghdl -a ../../Komponenten/ALU/my_gen_or.vhdl
-ghdl -a ../../Komponenten/ALU/my_gen_and.vhdl
+ghdl -a --std=08 ../../Komponenten/ALU/my_alu.vhdl
 
-ghdl -a ../../Komponenten/ALU/my_alu.vhdl
-
-ghdl -a my_alu_tb.vhdl
-ghdl -e my_alu_tb
-ghdl -r my_alu_tb --vcd=my_alu_tb.vcd --stop-time=200ns
+ghdl -a --std=08 my_alu_tb.vhdl
+ghdl -e --std=08 my_alu_tb
+ghdl -r --std=08 my_alu_tb --vcd=my_alu_tb.vcd --stop-time=200ns
